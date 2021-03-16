ed44fce5a5b56b6121175c2990e1d1e32d2feacd
Eirik Lorgen Tanberg <eirik@tanberg.org>
2021-03-16 11:03:37 +0100

    Add support for using environment variables to configure database

--- src/main/java/no/ntnu/JavaLauncher.java
+++ src/main/java/no/ntnu/JavaLauncher.java
@@ -10,10 +10,25 @@ public class JavaLauncher {
     public static void main(String[] args) {
         App app = new App();
 
+        String url, username, password;
+
         if (args.length >= 3) {
-            String url = args[0];
-            String username = args[1];
-            String password = args[2];
+            System.out.println("Accepting database configuration from command line...");
+            url = args[0];
+            username = args[1];
+            password = args[2];
+        } else {
+            System.out.println("Accepting database configuration from environment...");
+            url = System.getenv("DB_URL");
+            username = System.getenv("DB_USERNAME");
+            password = System.getenv("DB_PASSWORD");
+        }
+
+        if (url != null && username != null && password != null) {
+            System.out.println("Using ");
+            System.out.println(" URL: " + url);
+            System.out.println(" Username: " + username);
+            System.out.println(" Password: " + password);
 
             app.setConnectionManager(new ConnectionManager(url, username, password));
             app.getConnectionManager().testConnection();
